a51234d246114b866152cbc948dddc39511022cf
SINDNURE <119412287+SINDNURE@users.noreply.github.com>
2022-11-29 13:40:40 +0300

    hilmi sevban erin  structunion.c

--- username_pass_func_struct.c
+++ username_pass_func_struct.c
@@ -1,32 +1,84 @@
+////create a username and password and check it with function
+//
+//#include "stdio.h"
+//#include "stdlib.h"
+//#include "string.h"
+//#include "ctype.h" // for isdigit()
+//
+//void login(char username[], char password[]) {
+//  
+//}
+//
+//int main() {
+//    char username[20];
+//    char password[20];
+//    printf("Enter your username: ");
+//    scanf("%s", username);
+//    printf("Enter your password: ");
+//    scanf("%s", password);
+//    login(username, password);
+//    return 0;
+//}
+#include <stdio.h>
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "ctype.h" // for isdigit()
+#include <string.h>
 
-struct login {
-    char username[20]; // 20 is the maximum length of the username and password that can be entered
-    char password[20];   // 20 is the maximum length of the username and password that can be entered
-};
+int main ()
 
-void login(struct login log) {
-    char username1[20]; // 20 is the maximum length of the username and password that can be entered
-    char password1[20];     // 20 is the maximum length of the username and password that can be entered
-    printf("Enter your username: ");
-    scanf("%s", username1);
-    printf("Enter your password: ");
-    scanf("%s", password1);
-    if (strcmp(log.username, username1) == 0 && strcmp(log.password, password1) == 0) {
-        printf("You are logged in");
-    } else {
-        printf("Wrong username or password");
-    }
+{
+
+char id[50];
+
+User:
+
+printf("Enter your id:\n");
+
+scanf("%s", &id);
+
+if (strcmp(id,"hilmi")==0)
+
+{
+
+printf("Id is correct\n");
 
 }
 
-int main() {
-// your code will be here
-  
-  
+else
+
+{
+
+printf("You have enetred an invalid user id\nPlease enter id again\n");
+
+goto User;
+
+}
+
+char pass[50];
+
+pass:
+
+printf("Enter your password: ");
+
+scanf("%s", &pass);
+
+if(strcmp(pass,"71")==0)
+
+{
+
+printf("You have successly loged in into G-Mail\n");
+
+}
+
+else
+
+{
+
+printf("You have entered a wrong password\nEnter your password again\n");
+
+goto pass;
+
+}
+
+return 0;
 
 }
